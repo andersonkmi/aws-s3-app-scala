a44150631a7cb68cfc612f98fb7f4b4dc91c7e1b
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-05 17:09:27 +0100

    more code

--- src/main/scala/org/codecraftlabs/s3app/Main.scala
+++ src/main/scala/org/codecraftlabs/s3app/Main.scala
@@ -1,9 +1,10 @@
 package org.codecraftlabs.s3app
 
-import org.codecraftlabs.s3app.data.S3Bucket
+import org.codecraftlabs.s3app.data.AwsRegion
+import org.codecraftlabs.s3app.service.S3BucketListService
 
 object Main extends App {
-  val s3Bucket: S3Bucket = new S3Bucket("teste")
-  println(s3Bucket.getName)
+  val list = S3BucketListService.list(AwsRegion.UsEast1)
+  list.get.foreach(item => println(item.getName))
   println("Done")
 }
