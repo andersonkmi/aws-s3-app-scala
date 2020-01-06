62b3d152cce08f43e076b5b8dc66092f2ec9513a
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-06 21:57:24 +0100

    more code

--- src/main/scala/org/codecraftlabs/s3app/Main.scala
+++ src/main/scala/org/codecraftlabs/s3app/Main.scala
@@ -2,14 +2,18 @@ package org.codecraftlabs.s3app
 
 import org.apache.logging.log4j.{LogManager, Logger}
 import org.codecraftlabs.s3app.data.AwsRegion
-import org.codecraftlabs.s3app.service.S3BucketListService
+import org.codecraftlabs.s3app.service.{AwsException, S3BucketListService}
 
 object Main extends App {
   @transient private lazy val logger: Logger = LogManager.getLogger(Main.getClass)
 
   logger.info("Starting app")
-  val list = S3BucketListService.list(AwsRegion.UsEast1)
-  list.get.foreach(logger.info)
+  try {
+    val list = S3BucketListService.list(AwsRegion.UsEast1)
+    list.get.foreach(logger.info)
+  } catch {
+    case exception: AwsException => logger.error("Error when calling AWS", exception)
+  }
 
   logger.info("Done")
 }
