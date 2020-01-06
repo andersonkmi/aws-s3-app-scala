1354889ccc8ab4bce63ff6f17d9de3e07ed52e56
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-06 21:37:20 +0100

    added log4j2 properties

--- src/main/scala/org/codecraftlabs/s3app/Main.scala
+++ src/main/scala/org/codecraftlabs/s3app/Main.scala
@@ -1,10 +1,15 @@
 package org.codecraftlabs.s3app
 
+import org.apache.logging.log4j.{LogManager, Logger}
 import org.codecraftlabs.s3app.data.AwsRegion
 import org.codecraftlabs.s3app.service.S3BucketListService
 
 object Main extends App {
+  @transient private lazy val logger: Logger = LogManager.getLogger(Main.getClass)
+
+  logger.info("Starting app")
   val list = S3BucketListService.list(AwsRegion.UsEast1)
-  list.get.foreach(item => println(item))
-  println("Done")
+  list.get.foreach(logger.info)
+
+  logger.info("Done")
 }
