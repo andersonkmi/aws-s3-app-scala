2925aad6faec4ec1c3ba3f0d17a7be782662d622
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-07 18:27:24 +0100

    more code

--- src/main/scala/org/codecraftlabs/s3app/util/ArgsValidatorUtil.scala
+++ src/main/scala/org/codecraftlabs/s3app/util/ArgsValidatorUtil.scala
@@ -1,7 +1,28 @@
 package org.codecraftlabs.s3app.util
 
+import org.codecraftlabs.s3app.util.ArgsUtils.{bucketName, regionName, serviceName}
+
 object ArgsValidatorUtil {
   def validateForBucketCreation(args: Map[String, String]): Unit = {
+    val serviceArg = args.getOrElse(serviceName, "")
+    val regionArg = args.getOrElse(regionName, "")
+    val bucketArg = args.getOrElse(bucketName, "")
 
+    if (serviceArg.isEmpty || regionArg.isEmpty || bucketArg.isEmpty) throw InvalidArgumentException("Missing args")
+  }
+
+  def validateForBucketListing(args: Map[String, String]): Unit = {
+    val serviceArg = args.getOrElse(serviceName, "")
+    val regionArg = args.getOrElse(regionName, "")
+
+    if (serviceArg.isEmpty || regionArg.isEmpty) throw InvalidArgumentException("Missing args")
+  }
+
+  def validateForBucketDelete(args: Map[String, String]): Unit = {
+    val serviceArg = args.getOrElse(serviceName, "")
+    val regionArg = args.getOrElse(regionName, "")
+    val bucketArg = args.getOrElse(bucketName, "")
+
+    if (serviceArg.isEmpty || regionArg.isEmpty || bucketArg.isEmpty) throw InvalidArgumentException("Missing args")
   }
 }
