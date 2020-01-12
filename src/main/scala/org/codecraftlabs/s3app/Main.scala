50d652ecaafd9babd313062d5ad83c0a6446a6d4
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-12 21:24:18 +0100

    more code

--- src/main/scala/org/codecraftlabs/s3app/Main.scala
+++ src/main/scala/org/codecraftlabs/s3app/Main.scala
@@ -1,14 +1,14 @@
 package org.codecraftlabs.s3app
 
 import org.apache.logging.log4j.{LogManager, Logger}
-import org.codecraftlabs.s3app.data.AwsRegion
+import org.codecraftlabs.s3app.data.{AwsRegion, S3Bucket}
 import org.codecraftlabs.s3app.data.AwsRegion.withNameOpt
 import org.codecraftlabs.s3app.service.S3BucketListService.list
-import org.codecraftlabs.s3app.service.AwsException
-import org.codecraftlabs.s3app.util.ArgsUtils.{parseArgs, regionName, serviceName}
+import org.codecraftlabs.s3app.service.{AwsException, S3BucketCreateService}
+import org.codecraftlabs.s3app.util.ArgsUtils.{bucketName, parseArgs, regionName, serviceName}
 import org.codecraftlabs.s3app.util.InvalidArgumentException
 import org.codecraftlabs.s3app.util.ArgsValidatorUtil.validate
-import org.codecraftlabs.s3app.util.ServiceType.S3_BUCKET_LIST_SERVICE
+import org.codecraftlabs.s3app.util.ServiceType.{S3_BUCKET_CREATE_SERVICE, S3_BUCKET_LIST_SERVICE}
 
 object Main extends App {
   @transient private lazy val logger: Logger = LogManager.getLogger(Main.getClass)
@@ -25,6 +25,12 @@ object Main extends App {
         val awsRegion = if(withNameOpt(region).isEmpty) AwsRegion.UsEast1 else withNameOpt(region).get
         val result = list(awsRegion)
         result.get.foreach(logger.info)
+      case S3_BUCKET_CREATE_SERVICE =>
+        val region: String = mappedArgs.getOrElse(regionName, "")
+        val awsRegion = if(withNameOpt(region).isEmpty) AwsRegion.UsEast1 else withNameOpt(region).get
+        val bucket: String = mappedArgs.getOrElse(bucketName, "")
+        val s3Bucket = new S3Bucket(bucket, awsRegion)
+        S3BucketCreateService.create(s3Bucket)
       case _ => logger.warn("Not implemented yet")
     }
   } catch {
