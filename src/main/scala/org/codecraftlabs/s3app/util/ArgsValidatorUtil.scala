a2a5cac864899153101cbd694127e7065adf86e2
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-15 22:25:43 +0100

    more code

--- src/main/scala/org/codecraftlabs/s3app/util/ArgsValidatorUtil.scala
+++ src/main/scala/org/codecraftlabs/s3app/util/ArgsValidatorUtil.scala
@@ -6,6 +6,7 @@ import org.codecraftlabs.s3app.util.ServiceType.{S3_BUCKET_CREATE_SERVICE, S3_BU
 
 object ArgsValidatorUtil {
   private val MIN_ARGS_SIZE = 2
+
   def validate(args: Map[String, String]): Unit = {
     if (args.size < MIN_ARGS_SIZE) throw InvalidArgumentException("Missing arguments")
 
@@ -17,6 +18,24 @@ object ArgsValidatorUtil {
       case S3_BUCKET_CREATE_SERVICE => validateForBucketCreate(args)
       case _ => throw InvalidArgumentException(s"Invalid or missing $serviceName argument")
     }
+  }
+
+  def validate(appArgs: AppArgument): Unit = {
+    val service = appArgs.service
+
+    service match {
+      case S3_BUCKET_LIST_SERVICE => validateAppArgsForBucketList(appArgs)
+      case _ => throw InvalidArgumentException(s"Invalid or missing $serviceName argument")
+    }
+  }
+
+  private def validateAppArgsForBucketList(args: AppArgument): Unit = {
+    if (args.service.isEmpty) throw InvalidArgumentException(s"Missing $serviceName argument")
+
+    if (args.region.isEmpty) throw InvalidArgumentException(s"Missing $regionName argument")
+
+    if (AwsRegion.withNameOpt(args.region).isEmpty) throw InvalidArgumentException(s"Invalid $regionName argument value")
+
   }
 
   private def validateForBucketCreate(args: Map[String, String]): Unit = {
