d7988e35683899c326881ba379cfcc514c55d27f
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-17 23:28:34 +0100

    more code

--- src/main/scala/org/codecraftlabs/s3app/Main.scala
+++ src/main/scala/org/codecraftlabs/s3app/Main.scala
@@ -7,8 +7,8 @@ import org.codecraftlabs.s3app.service.S3BucketCreateService.create
 import org.codecraftlabs.s3app.service.S3BucketDeleteService.delete
 import org.codecraftlabs.s3app.service.S3BucketListService.list
 import org.codecraftlabs.s3app.service.AwsException
-import org.codecraftlabs.s3app.util.ArgsUtils.{bucketName, parseArgs, regionName, serviceName}
-import org.codecraftlabs.s3app.util.InvalidArgumentException
+import org.codecraftlabs.s3app.util.ArgsUtils.{bucketName, buildArg, parseArgs, regionName, serviceName}
+import org.codecraftlabs.s3app.util.{AppArgument, ArgsUtils, InvalidArgumentException}
 import org.codecraftlabs.s3app.util.ArgsValidatorUtil.validate
 import org.codecraftlabs.s3app.util.ServiceType.{S3_BUCKET_CREATE_SERVICE, S3_BUCKET_DELETE_SERVICE, S3_BUCKET_LIST_SERVICE}
 
@@ -19,13 +19,14 @@ object Main {
     logger.info("Starting app")
     try {
       val mappedArgs = parseArgs(args)
-      validate(mappedArgs)
+      val appArgs = buildArg(mappedArgs)
+      validate(appArgs)
 
       val selectedService: String = mappedArgs.getOrElse(serviceName, "")
       selectedService match {
-        case S3_BUCKET_LIST_SERVICE => listBucketService(mappedArgs)
-        case S3_BUCKET_CREATE_SERVICE => createBucketService(mappedArgs)
-        case S3_BUCKET_DELETE_SERVICE => deleteBucketService(mappedArgs)
+        case S3_BUCKET_LIST_SERVICE => listBucketService(appArgs)
+        case S3_BUCKET_CREATE_SERVICE => createBucketService(appArgs)
+        case S3_BUCKET_DELETE_SERVICE => deleteBucketService(appArgs)
         case _ => logger.warn("Not implemented yet")
       }
     } catch {
@@ -36,26 +37,26 @@ object Main {
     logger.info("Done")
   }
 
-  private def listBucketService(args: Map[String, String]): Unit = {
-    val region: String = args.getOrElse(regionName, "")
+  private def listBucketService(args: AppArgument): Unit = {
+    val region: String = args.region
     val awsRegion = if (withNameOpt(region).isEmpty) AwsRegion.UsEast1 else withNameOpt(region).get
     val result = list(awsRegion)
     result.get.foreach(logger.info)
     result.get.foreach(println)
   }
 
-  private def createBucketService(args: Map[String, String]): Unit = {
-    val region: String = args.getOrElse(regionName, "")
+  private def createBucketService(args: AppArgument): Unit = {
+    val region: String = args.region
     val awsRegion = if (withNameOpt(region).isEmpty) AwsRegion.UsEast1 else withNameOpt(region).get
-    val bucket: String = args.getOrElse(bucketName, "")
+    val bucket: String = args.bucket
     val s3Bucket = new S3Bucket(bucket, awsRegion)
     create(s3Bucket)
   }
 
-  private def deleteBucketService(args: Map[String, String]): Unit = {
-    val region: String = args.getOrElse(regionName, "")
+  private def deleteBucketService(args: AppArgument): Unit = {
+    val region: String = args.region
     val awsRegion = if (withNameOpt(region).isEmpty) AwsRegion.UsEast1 else withNameOpt(region).get
-    val bucket: String = args.getOrElse(bucketName, "")
+    val bucket: String = args.bucket
     val s3Bucket = new S3Bucket(bucket, awsRegion)
     delete(s3Bucket)
   }
